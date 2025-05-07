882249748a91bc1dcd03603ece32da8cf68a38db
Tayte Boss <boss.tayte@gmail.com>
2025-05-07 13:38:07 +0100

    updates

--- components/common/MediaStack/index.tsx
+++ components/common/MediaStack/index.tsx
@@ -1,45 +1,67 @@
-import styled from 'styled-components';
-import { useInView } from 'react-intersection-observer';
-import ImageComponent from './ImageComponent';
-import VideoComponent from './VideoComponent';
-import { MediaType } from '../../../shared/types/types';
+import styled from "styled-components";
+import { useInView } from "react-intersection-observer";
+import ImageComponent from "./ImageComponent";
+import VideoComponent from "./VideoComponent";
+import { MediaType } from "../../../shared/types/types";
 
 const MediaStackWrapper = styled.div``;
 
 type Props = {
-	data: MediaType;
-	isPriority?: boolean;
+  data: MediaType;
+  isPriority?: boolean;
+  noAnimation?: boolean;
+  sizes?: undefined | string;
+  alt?: string;
+  lazyLoad?: boolean;
+  useLoader?: boolean;
 };
 
 const MediaStack = (props: Props) => {
-	const { data, isPriority = false } = props ?? {};
+  const {
+    data,
+    isPriority = false,
+    noAnimation = false,
+    sizes = undefined,
+    alt,
+    lazyLoad = false,
+    useLoader = false,
+  } = props ?? {};
 
-	const useVideo = data?.mediaType === 'video';
+  // sizes="(max-width: 768px) 38vw, (max-width: 1024px) 20vw, 15vw"
 
-	const { ref, inView } = useInView({
-		triggerOnce: true,
-		threshold: 0.2,
-		rootMargin: '-5%'
-	});
+  const useVideo = data?.media?.mediaType === "video";
 
-	return (
-		<MediaStackWrapper ref={ref}>
-			{useVideo && (
-				<VideoComponent
-					data={data}
-					inView={inView}
-					isPriority={isPriority}
-				/>
-			)}
-			{!useVideo && (
-				<ImageComponent
-					data={data}
-					isPriority={isPriority}
-					inView={inView}
-				/>
-			)}
-		</MediaStackWrapper>
-	);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+    rootMargin: "-5%",
+  });
+
+  return (
+    <MediaStackWrapper ref={ref}>
+      {useVideo && (
+        <VideoComponent
+          data={data}
+          inView={inView}
+          isPriority={isPriority}
+          noAnimation={noAnimation}
+          lazyLoad={lazyLoad}
+          useLoader={useLoader}
+        />
+      )}
+      {!useVideo && (
+        <ImageComponent
+          data={data}
+          isPriority={isPriority}
+          inView={inView}
+          noAnimation={noAnimation}
+          sizes={sizes}
+          alt={alt}
+          lazyLoad={lazyLoad}
+        />
+      )}
+    </MediaStackWrapper>
+  );
 };
 
 export default MediaStack;
